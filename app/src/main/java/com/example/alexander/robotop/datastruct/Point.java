ca6d0bf447705b3bd485464b57c5b4b97bf4a238
michael-kaiser <michael.kaiser@student.uibk.ac.at>
2015-04-22 15:57:04 +0200

    corrected degrees

--- app/src/main/java/com/example/alexander/robotop/datastruct/Point.java
+++ app/src/main/java/com/example/alexander/robotop/datastruct/Point.java
@@ -69,7 +69,7 @@ public class Point {
         else if(triLength < 0 && triHight < 0){
             return (int)(Math.toDegrees(Math.atan(triHight/ triLength)) +180);
         }else {
-            return (int) -Math.toDegrees(Math.atin(triHight / -triLength));
+            return (int) -Math.toDegrees(Math.atan(triHight / -triLength));
         }
     }
 
