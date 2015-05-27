5ddb05550339b9692a87d7495e5b8ff4c64035f1
Ley789 <A.Lochmann@student.uibk.ac.at>
2015-05-27 15:05:08 +0200

    added Detector in ball detection

--- app/src/main/java/com/example/alexander/robotop/visualOrientation/DetectBalls.java
+++ app/src/main/java/com/example/alexander/robotop/visualOrientation/DetectBalls.java
@@ -4,6 +4,7 @@ package com.example.alexander.robotop.visualOrientation;
  * Created by Alexander on 28/04/2015.
  */
 import com.example.alexander.robotop.datastruct.ColorBound;
+import com.example.alexander.robotop.modell.Detector;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
@@ -21,7 +22,7 @@ public class DetectBalls implements Callable<Mat>{
 
     }
     /**
-     * Returns a mat of circles of red balls
+     * Returns a mat of circles of balls
      */
     @Override
     public Mat call() throws Exception {
@@ -31,16 +32,13 @@ public class DetectBalls implements Callable<Mat>{
         Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5,5));
         Imgproc.cvtColor(mRgba, mHSV, Imgproc.COLOR_RGB2HSV_FULL);
 
-        Core.inRange(mHSV, color.getmLowerBound(), color.getmUpperBound(), res);
-
+        res = Detector.detectBlob(mHSV,color);
         //morphological closing (fill small holes in the foreground)
         Imgproc.dilate( res, res, erodeElement );
         Imgproc.erode(res, res, erodeElement);
 
         Imgproc.GaussianBlur(res, res, new Size(9,9),0,0);
 
-        //Imgproc.erode(res, res, erodeElement);
-        //Imgproc.erode(res, res, erodeElement);
         Imgproc.HoughCircles(res, circles, Imgproc.CV_HOUGH_GRADIENT, 2, res.height()/4,100, 70, 0, 150);
         if(circles.rows() > 0) {
             return circles;
